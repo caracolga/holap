60862d3eaaa4022dcc5549137e575c983fee9138
Olga F. Vio <49153772+caracolga@users.noreply.github.com>
2020-03-11 11:28:43 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var a = 50, b = 80;
+var c;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  colorMode(HSB, 360, 100, 100);
+  //comentario de una línea
+  /*comentarios de 
+  más líneas */
+  //background(0,100,100);
+  fill(random (0,360),100,100);
+  noStroke(); 
+  c = a * random (1,2);
+  ellipse(mouseX,mouseY,c,c);
+  frameRate (10)
+  
+  
+}
